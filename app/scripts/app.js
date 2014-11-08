34242513001b108ecfc63e534a34c248b993769f
Mara Kim <hacker.root@gmail.com>
2014-11-08 14:07:56 -0600

    Add feed to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,18 @@ angular.module('CitizenApp', ['ngRoute'])
       templateUrl: '/templates/home.html',
       controller: 'HomeCtrl'
     })
+    .when('/feed', {
+      templateUrl: '/templates/feed.html'
+    })
+    .when('/reps', {
+      templateUrl: '/templates/reps.html'
+    })
+    .when('/links', {
+      templateUrl: '/templates/links.html'
+    })
+    .when('/about', {
+      templateUrl: '/templates/about.html'
+    })
     .when('/officialsList', {
       templateUrl: '/templates/officialsList.html',
       controller: 'OfficialsListCtrl'
