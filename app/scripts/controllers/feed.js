8d36edf7cf9f6a54d905273e0b24b31be2b3bd1c
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2014-11-08 20:50:18 -0600

    Add infinite-scroll to feed.html

---
+++ app/scripts/controllers/feed.js
@@ -0,0 +1,20 @@
+angular.module('CitizenApp').controller('FeedController', function ($scope) {
+  $scope.items = [];
+  $scope.canLoad = true;
+  $scope.maxItems = 100;
+  $scope.addItems = function () {
+    for (var i = 0; i < 10; i++) {
+      $scope.items.push({name:'item ' + ($scope.items.length + 1)});
+      if ($scope.items.length >= $scope.maxItems) {
+        $scope.canLoad = false;
+        return;
+      }
+    }
+  };
+  $scope.reset = function () {
+    $scope.items = [];
+    $scope.canLoad = true;
+    $scope.addItems();
+  };
+  $scope.reset();
+});
