84d7382e0573956395a95b9cc549330adab92d11
Mara Kim <hacker.root@gmail.com>
2014-11-11 14:04:20 -0600

    Partial fix of race condition

--- app/scripts/services/map.js
+++ app/scripts/services/map.js
@@ -17,23 +17,27 @@ angular.module('CitizenApp')
     env = {paper:paper,viewbox:viewbox};
   });
 
-  var prev_color = [];
+  var prev_id = [], color_map = {};
   map.focus = function (id) {
     if(id) {
-      if(prev_color.length) {
-        var info = prev_color.pop();
-        env.paper.select('#'+info.id).attr({
-            fill: info.color});
+      if(prev_id.length) {
+        env.paper.select(prev_id.pop()).attr({
+            fill: color_map[last_id]
+        });
       }
       var state = env.paper.select('#'+id);
-      prev_color.push({id:id,color: state.attr('fill')});
+      if(!color_map[id]) {
+          color_map[id] = state.attr('fill');
+      }
+      prev_id.push(id);
       var start = env.paper.attr('viewBox').vb,
         bbox = state.getBBox(),
         scale = .2,
         end = (bbox.x-scale/2*bbox.width) + ' ' + (bbox.y-scale/2*bbox.height) + ' '
           + (bbox.width+scale*bbox.width) + ' ' + (bbox.height+scale*bbox.height);
-      console.log(end, state.getBBox());
-      state.animate({fill: '#F9B099'},1000,mina.easein);
+      state.animate({fill: '#F9B099'},1000,mina.easeout);
+        var last_state = env.paper.select(prev_id.pop());
+        last_state.animate({fill: color_map[last_id]},1000,mina.easeout);
       env.paper.add(Snap.parse(
         '<animate id="smoothpan" attributeName="viewBox" dur="4s" values="'
           + start + ';' + end + '" fill="freeze" />'));
