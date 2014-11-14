2cbe46be1c34ff46fa905b87e870899ac9247a01
Mara Kim <hacker.root@gmail.com>
2014-11-14 01:06:32 -0600

    Rich target matching

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 npm install &&
-if [ -z "$*" ]
+if [[ -z "$*" || "server" == "$1"* ]]
 then
   node node_modules/gulp/bin/gulp.js watch &
   node web.js
-elif [ "$1" = "build" ]
+elif [[ "build" == "$1"* ]]
 then
   node node_modules/gulp/bin/gulp.js
+else
+  printf 'Unrecognized option: `%s`
+
+Usage: %s [TARGET]
+Where TARGET (partially) matches:
+  server	Run app watching for changes (default)
+  build		Build app for deployment
+' "$1" "$0"
 fi
