0a5a40de3a99a14254e5b34e4c065a6f91f02232
Mara Kim <hacker.root@gmail.com>
2014-11-17 00:41:06 -0600

    Stronger gulp

--- run.sh
+++ run.sh
@@ -4,22 +4,4 @@ GULP="$(which gulp)"
 GULP="${GULP:-node_modules/gulp/bin/gulp.js}"
 
 npm install &&
-if [[ -z "$*" || "server" == "$1"* ]]
-then
-  $GULP
-elif [[ "build" == "$1"* ]]
-then
-  $GULP build
-elif [[ "deploy" == "$1"* ]]
-then
-  $GULP deploy
-else
-  printf 'Unrecognized option: `%s`
-
-Usage: %s [GULP_TARGET]
-Where GULP_TARGET matches:
-  server	Run app watching for changes (default)
-  build		Build app
-  deploy	Build and deploy to gh-pages
-' "$1" "$0"
-fi
+$GULP "$@"
